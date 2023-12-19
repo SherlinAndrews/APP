3cf0d7c73aa8dcf6baf9d397240190c9716e52a1
SherlinAndrews <154241404+SherlinAndrews@users.noreply.github.com>
2023-12-19 15:30:50 +0530

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,8 @@
+
+export const groupBy = (array, fn) =>
+  array.reduce((result, item) => {
+    const key = fn(item)
+    if (!result[key]) result[key] = []
+    result[key].push(item)
+    return result
+  }, {})
